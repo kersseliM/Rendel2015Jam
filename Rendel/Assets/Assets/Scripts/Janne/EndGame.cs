80b4bb0a4ebc7c64d0f8ff16ce9abf30f9d9f9d9
kazi7 <jannelain@gmail.com>
2015-09-05 16:44:28 +0300

    Canvas Update, Script Update

---
+++ Rendel/Assets/Assets/Scripts/Janne/EndGame.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EndGame : MonoBehaviour
+{
+    string name = "";
+    string score = "";
+    List<Scores> highScore;
+
+    void Start()
+    {
+
+    }
+
+    public void AddScore()
+    {
+        HighScoreManager._instance.SaveHighScore(name, System.Int32.Parse(score));
+        highScore = HighScoreManager._instance.GetHighScore();
+    }
+
+    public void GetLeaderboard()
+    {
+        highScore = HighScoreManager._instance.GetHighScore();
+    }
+
+    public void Get
+}
