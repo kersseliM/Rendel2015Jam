e57a2ae5ab2cef210b4d579e638cfc7c266e4a69
kersseliM <ttk14smarkoj@kamk.fi>
2015-09-05 14:33:46 +0300

    trrt

--- Rendel/Assets/ExplosionForce.cs
+++ Rendel/Assets/ExplosionForce.cs
@@ -7,9 +7,11 @@ public class ExplosionForce : MonoBehaviour
     public float timeToGoOff;
     PointEffector2D pe;
     public float säätö;
-
+    CircleCollider2D circleCol;
+    public float circleSäätö=0.25f;
     void Awake()
     {
+        circleCol = GetComponent<CircleCollider2D>();
         pe = GetComponent<PointEffector2D>();
     }
     void OnEnable()
@@ -17,6 +19,7 @@ public class ExplosionForce : MonoBehaviour
         Invoke("a", timeToGoOff);
         if(Global.Instance != null)
         pe.forceMagnitude = Global.Instance.totalForce *säätö ;
+      //  circleCol.radius = Global.Instance.totalForce * circleSäätö;
     }
     void a()
     {
