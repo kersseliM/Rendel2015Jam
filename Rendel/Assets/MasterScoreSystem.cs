ed52203a5c2ef2286e5e9aa1b5aae942f4f28a17
kersseliM <ttk14smarkoj@kamk.fi>
2015-09-05 14:03:29 +0300

    ScoreEffect

---
+++ Rendel/Assets/MasterScoreSystem.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class MasterScoreSystem : MonoBehaviour 
+{
+    public Text scoreText;
+    List<GameObject> effects = new List<GameObject>();
+    public static MasterScoreSystem Instanse;
+    public int poolLenght = 500;
+
+    int currentEffect;
+    int currentScore;
+    int visibleScore;
+    public GameObject scoreEffect;
+    public int oneScoreValue = 100;
+	// Use this for initialization
+	
+    void Awake()
+    {
+        Instanse = this;
+    }
+    
+    void Start () 
+    {
+	
+        for(int i =0 ; i<poolLenght;i++)
+        {
+            Instantiate(scoreEffect, scoreEffect.transform.position, transform.rotation);
+        }
+
+	}
+    public void addMeToList(GameObject g)
+    {
+        effects.Add(g);
+    }
+	
+
+
+
+    public void setEffect(Vector3 pos)
+    {
+        effects[currentEffect].transform.position = pos;
+        effects[currentEffect].SetActive(true);
+        currentEffect++;
+        if (currentEffect >= poolLenght)
+            currentEffect = 0;
+        currentScore += oneScoreValue;
+    }
+
+
+    void Update()
+    {
+        visibleScore = (int)Mathf.Lerp(visibleScore, currentScore, Time.deltaTime * lerpSpeed);
+        scoreText.text = visibleScore.ToString();
+
+    }
+    public float lerpSpeed = 4;
+
+
+
+
+
+}
---
+++ Rendel/Assets/ScoreEffect.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreEffect : MonoBehaviour 
+{
+
+    float  targetAlfa=0;
+   float startAlfa=1;
+    float time;
+    float timeTakenToLerp = 1;
+    bool isLerping;
+    SpriteRenderer sr;
+   
+
+    void Awake()
+    {
+        MasterScoreSystem.Instanse.addMeToList(gameObject);
+    }
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+
+    void OnEnable()
+    {
+        startLerp();
+    }
+
+    void FixedUpdate()
+    {
+        if (isLerping)
+        {
+            float timeSinceStarted = Time.time - time;
+            float percentage = timeSinceStarted / timeTakenToLerp;
+
+            Color c = sr.color;
+            c.a = Mathf.Lerp(startAlfa, targetAlfa, percentage);
+            sr.color = c;
+
+            if (percentage >= 1)
+            {
+                isLerping = false;
+               // Global.Instance.setWorldState(eStates.PowerSwiping);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "TaloPala" || col.gameObject.tag == "Talo")
+            Global.Instance.setExplosion(col.transform.position + Vector3.right);
+    }
+
+    public void startLerp()
+    {
+        isLerping = true;
+        time = Time.time;
+    }
+}
