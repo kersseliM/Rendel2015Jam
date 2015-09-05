33d67013af12384b113f09763f0f0653428741e4
kazi7 <jannelain@gmail.com>
2015-09-05 10:40:32 +0300

    SwipingPhase Done

--- Rendel/Assets/Assets/Scripts/Janne/PowerSwipe.cs
+++ Rendel/Assets/Assets/Scripts/Janne/PowerSwipe.cs
@@ -1,11 +1,154 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PowerSwipe : MonoBehaviour
 {
+    enum SwipeDirection
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
     Vector2 firstPressPos;
     Vector2 secondPressPos;
     Vector2 currentSwipe;
+
+    public Rigidbody2D VALAman;
+    public Text Direction;
+    public Text PointsText;
+    public Text PowerText;
+    public Text InfoText;
+    public Text timeLeftText;
+
+    public float IntroDelay = 4;
+    public float SwipeTime = 5;
+    public int PowerValue = 5;
+
+    int currentAction;
+    string playerInputAction = " ";
+    string upAction = "Up";
+    string downAction = "Down";
+    string leftAction = "Left";
+    string rightAction = "Right";
+
+    float swipeTimer;   
+    float gameTime;
+    float randomizer;
+    SwipeDirection randomSwipeDir;
+    string givenSwipeDir;
+    bool giveNewDirection;
+    bool startSwipeTimer;
+
+    int power;
+
+    void Start()
+    {
+        swipeTimer = SwipeTime;
+        giveNewDirection = true;
+        startSwipeTimer = false;
+    }
+
+    void Update()
+    {
+        if (Global.Instance.gameState == eStates.PowerSwiping)
+        {
+            updateTimers();
+            intro();
+            if (gameTime > IntroDelay)
+            {
+                startSwipeTimer = true;
+                if (giveNewDirection == true)
+                {
+                    randomSwipeDir = getRandomEnum<SwipeDirection>();
+                    givenSwipeDir = randomSwipeDir.ToString();
+                    Direction.text = givenSwipeDir;
+                    giveNewDirection = false;
+                }
+            }
+
+            if (swipeTimer > 0 && startSwipeTimer == true)
+            {
+                Swipe();
+                bool tempBool = isCorrectDirection();
+                if (tempBool)
+                {
+                    power += PowerValue;
+                    giveNewDirection = true;
+                }
+            }
+            if (swipeTimer <= 0)
+            {
+                startNextPhase();
+            }
+        }      
+    }
+
+    void LateUpdate()
+    {
+        timeLeftText.text = Mathf.Round(swipeTimer).ToString();      
+        PowerText.text = power.ToString();
+    }
+
+    void intro()
+    {
+        if (gameTime >= 0 && gameTime < 3)
+        {
+            InfoText.text = "READY";
+        }
+        if (gameTime >= 3 && gameTime < 4)
+        {
+            InfoText.text = "GO";
+        }
+        if (gameTime >= 4)
+        {
+            InfoText.text = "";
+        }
+    }
+
+    void updateTimers()
+    {
+        if (startSwipeTimer == true)
+        {
+            swipeTimer -= 1 * Time.deltaTime;
+        }
+
+        if (swipeTimer <= 0)
+        {
+            swipeTimer = 0;
+        }
+        gameTime += 1 * Time.deltaTime;
+    }
+
+    bool isCorrectDirection()
+    {
+        if (playerInputAction == givenSwipeDir)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    void resetCurrentState()
+    {
+        gameTime = 0;
+    }
+
+    void startNextPhase()
+    {
+        Global.Instance.setWorldState(eStates.AngleSwiping);
+    }
+
+    static T getRandomEnum<T>()
+    {
+        System.Array arr = System.Enum.GetValues(typeof(T));
+        T randomValue = (T)arr.GetValue(UnityEngine.Random.Range(0, arr.Length));
+        return randomValue;
+    }
 
 #if UNITY_ANDROID
     public void Swipe()
@@ -89,23 +232,4 @@ public class PowerSwipe : MonoBehaviour
         }
     }
 #endif
-
-    int currentAction;
-    char playerInputAction = ' ';
-    char upAction = 'u';
-    char downAction = 'd';
-    char leftAction = 'l';
-    char rightAction = 'r';
-
-    float timer;
-
-    void Start()
-    {
-        timer += 1 * Time.deltaTime;
-    }
-
-    void Update()
-    {
-        Swipe();
-    }
 }
